f314b7ab8dee2d747c39ff89d0776219c7336c69
RadenDemas <radendemas21@gmail.com>
2026-07-29 08:06:07 +0700

    Adding login and register using Identity code generator and update some conf

--- Program.cs
+++ Program.cs
@@ -1,29 +1,123 @@
+using EDIITechincalInterview.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// Database
+var connectionString = builder.Configuration
+    .GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
+    ));
+
+
+// ASP.NET Core Identity
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        // Account
+        options.SignIn.RequireConfirmedAccount = false;
+
+        // Password
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+// Cookie Authentication
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+
+    options.ExpireTimeSpan = TimeSpan.FromHours(6);
+    options.SlidingExpiration = true;
+
+    options.LoginPath = "/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+});
+
+
+// Rate Limiting
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("Fixed", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromSeconds(10);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+});
+
+
+// MVC
+builder.Services.AddControllersWithViews(options =>
+{
+    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+});
+
+// Routing Pages
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AddPageRoute(
+        "/Account/Login",
+        "/login"
+    );    
+
+    options.Conventions.AddPageRoute(
+        "/Account/Register",
+        "/register"
+    );
+});
+
+
+// Build Application
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+// HTTP Request Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
 app.UseRouting();
+
+app.UseRateLimiter();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
+
+// Routing
 app.MapStaticAssets();
 
+// Default MVC Route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+// ASP.NET Core Identity Razor Pages
+app.MapRazorPages();
 
 app.Run();
